d1a37f577982c7691f0b2f5eb2a869849edca72c
lhmchyd <75568532+lhmchyd@users.noreply.github.com>
2024-07-01 19:05:43 +0700

    Update copy_url.js

--- copy_url.js
+++ copy_url.js
@@ -1,10 +1,15 @@
 // Function to copy the current URL to clipboard
 function copyCurrentUrl() {
-  var currentUrl = window.location.href; // Get the current URL
-  navigator.clipboard.writeText(currentUrl); // Copy URL to clipboard
-
-  // Optionally, provide some user feedback
-  alert("Copied to clipboard: " + currentUrl);
+    var currentUrl = window.location.href; // Get the current URL
+    navigator.clipboard.writeText(currentUrl)
+        .then(function() {
+            console.log('URL copied to clipboard');
+            utils.snackbarShow('URL copied to clipboard', false, 2000); // Show snackbar notification
+        })
+        .catch(function(err) {
+            console.error('Failed to copy: ', err);
+            utils.snackbarShow('Failed to copy URL to clipboard. Please try again', true, 2000); // Show snackbar for failure
+        });
 }
 
 // Attach the copyCurrentUrl function to the element with ID 'menu-copyUrl'
