2f96d193e327996260d5585bcc44fb3b6937f85a
lhmchyd <75568532+lhmchyd@users.noreply.github.com>
2024-07-11 18:53:55 +0700

    Delete prefix.js

--- prefix.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  document.querySelectorAll('.fa').forEach(icon => {
-    icon.classList.remove('fa', 'fas', 'far', 'fab', 'solitude');
-    icon.classList.add('kf');
-    const iconClass = icon.classList.item(1); // e.g., 'fa-home'
-    const newIconClass = iconClass.replace('fa-', 'kf-'); // Replace 'fa-' with 'kf-'
-    icon.classList.add(newIconClass);
-  });
-});
